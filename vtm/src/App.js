59034875662a878471536b7d997ed02bab6de442
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2023-11-15 02:45:19 +0530

    navbar updated

--- vtm/src/App.js
+++ vtm/src/App.js
@@ -1,25 +1,201 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState,useEffect } from "react";
+import logo from "./Images/Logo1.png";
+import "./Navbar.css";
+import { Link } from 'react-router-dom'
 
-function App() {
+
+export default function Navbar() {
+  const closeMobileMenu = () => {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    if (navbarToggler) {
+      // console.log("aniket");
+      navbarToggler.click(); // Simulate a click on the mobile menu toggle button
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand mx-4">
+            <Link to="/">
+            <img src={logo} id="logo" />
+            </Link>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+           
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul
+              className="navbar-nav me-auto mb-2 mb-lg-0 mx-4"
+              id="ulContainer"
+            >
+                <li className="nav-item navbarlink">
+                <a
+                  className="nav-link active font "
+                  id="navlink"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home<i className='bx bx-caret-down'></i>
+                </a>
+                {/* <ul className="dropdown" id="dropdown_text">
+                  <li>
+                    <Link to="CCNA"onClick={closeMobileMenu}>CCNA 200-301</Link>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>CCNA Enterprises</a>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>OSPF|BGP|MPLS</a>
+                  </li>
+                </ul> */}
+              </li>
+              <li className="nav-item navbarlink">
+                <a
+                  className="nav-link active font "
+                  id="navlink"
+                  aria-current="page"
+                  href="#"
+                >
+                  Courses<i className='bx bx-caret-down'></i>
+                </a>
+                {/* <ul className="dropdown" id="dropdown_text">
+                  <li>
+                    <Link to="CCNA"onClick={closeMobileMenu}>CCNA 200-301</Link>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>CCNA Enterprises</a>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>OSPF|BGP|MPLS</a>
+                  </li>
+                </ul> */}
+              </li>
+              <li className="nav-item navbarlink">
+                <a
+                  className="nav-link active font "
+                  id="navlink"
+                  aria-current="page"
+                  href="#"
+                >
+                  Page Layout<i className='bx bx-caret-down'></i>
+                </a>
+                {/* <ul className="dropdown" id="dropdown_text">
+                  <li>
+                    <Link to="CCNA"onClick={closeMobileMenu}>CCNA 200-301</Link>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>CCNA Enterprises</a>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>OSPF|BGP|MPLS</a>
+                  </li>
+                </ul> */}
+              </li>
+              
+              <li className="nav-item navbarlink">
+                <a
+                  className="nav-link active font "
+                  id="navlink"
+                  aria-current="page"
+                  href="#"
+                >
+                  Our Team<i className='bx bx-caret-down'></i>
+                </a>
+                {/* <ul className="dropdown" id="dropdown_text">
+                  <li>
+                    <Link to="CCNA"onClick={closeMobileMenu}>CCNA 200-301</Link>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>CCNA Enterprises</a>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>OSPF|BGP|MPLS</a>
+                  </li>
+                </ul> */}
+              </li>
+              <li className="nav-item navbarlink">
+                <a
+                  className="nav-link active font "
+                  id="navlink"
+                  aria-current="page"
+                  href="#"
+                >
+                  Gallery<i className='bx bx-caret-down'></i>
+                </a>
+                {/* <ul className="dropdown" id="dropdown_text">
+                  <li>
+                    <Link to="CCNA"onClick={closeMobileMenu}>CCNA 200-301</Link>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>CCNA Enterprises</a>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>OSPF|BGP|MPLS</a>
+                  </li>
+                </ul> */}
+              </li>
+              <li className="nav-item navbarlink">
+                <a
+                  className="nav-link active font "
+                  id="navlink"
+                  aria-current="page"
+                  href="#"
+                >
+                  Profile<i className='bx bx-caret-down'></i>
+                </a>
+                {/* <ul className="dropdown" id="dropdown_text">
+                  <li>
+                    <Link to="CCNA"onClick={closeMobileMenu}>CCNA 200-301</Link>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>CCNA Enterprises</a>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>OSPF|BGP|MPLS</a>
+                  </li>
+                </ul> */}
+              </li>
+              <li className="nav-item navbarlink">
+                <a
+                  className="nav-link active font "
+                  id="navlink"
+                  aria-current="page"
+                  href="#"
+                >
+                  Contact<i className='bx bx-caret-down'></i>
+                </a>
+                {/* <ul className="dropdown" id="dropdown_text">
+                  <li>
+                    <Link to="CCNA"onClick={closeMobileMenu}>CCNA 200-301</Link>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>CCNA Enterprises</a>
+                  </li>
+                  <li>
+                    <a href="#"onClick={closeMobileMenu}>OSPF|BGP|MPLS</a>
+                  </li>
+                </ul> */}
+              </li>
+          
+              
+            
+            </ul>
+          </div>
+
+          
+        </div>
+      </nav>
+      
+    </>
   );
 }
-
-export default App;
---
+++ vtm/src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <>
+   
+    <BrowserRouter>
+        <Navbar />
+       
+
+      
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
